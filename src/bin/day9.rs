c8f54cfe4d33cf2405c75a33b69a6ed13a47e6eb
Samuel Falk <falkwastaken@gmail.com>
2022-12-10 13:59:00 +0100

    Day 9

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -1,98 +1,108 @@
 use itertools::Itertools;
-use std::{collections::HashSet, ops::Add};
+use std::{
+    collections::HashSet,
+    ops::{Add, AddAssign, Sub},
+};
 
-const TEST: &str = 
-"R 4
-U 4
-L 3
-D 1
-R 4
-D 1
-L 5
-R 2";
+#[derive(Default, PartialEq, Eq, Hash, Clone, Copy)]
+struct Pos(isize, isize);
 
-type Pos = (isize, isize);
-
-struct Move(isize, isize);
-
-impl From<&str> for Move {
+impl From<&str> for Pos {
     fn from(line: &str) -> Self {
         let (dir, steps) = line.split_once(" ").unwrap();
         let steps = steps.parse().unwrap();
         match dir {
-            "R" => Move(0, steps),
-            "L" => Move(0, -steps),
-            "U" => Move(steps, 0),
-            "D" => Move(-steps, 0),
-            _ => panic!("Invalid direction!")
+            "R" => Pos(steps, 0),
+            "L" => Pos(-steps, 0),
+            "U" => Pos(0, steps),
+            "D" => Pos(0, -steps),
+            _ => panic!("Invalid direction!"),
         }
+    }
+}
+
+impl Add<Pos> for Pos {
+    type Output = Pos;
+    fn add(self, rhs: Pos) -> Self::Output {
+        Pos(self.0 + rhs.0, self.1 + rhs.1)
+    }
+}
+
+impl AddAssign<Pos> for Pos {
+    fn add_assign(&mut self, rhs: Pos) {
+        self.0 += rhs.0;
+        self.1 += rhs.1;
+    }
+}
+
+impl Sub<Pos> for Pos {
+    type Output = Pos;
+    fn sub(self, rhs: Pos) -> Self::Output {
+        Pos(self.0 - rhs.0, self.1 - rhs.1)
+    }
+}
+
+impl Pos {
+    fn abs(&self) -> isize {
+        self.0.abs() + self.1.abs()
+    }
+
+    fn unit(&self) -> Pos {
+        Pos(self.0.signum(), self.1.signum())
     }
 }
 
 struct Rope {
-    head: Pos,
-    tail: Pos,
-    visited: HashSet<Pos>
+    knots: Vec<Pos>,
+    visited: HashSet<Pos>,
 }
 
 impl Rope {
-    fn new() -> Rope {
+    fn new(n: usize) -> Rope {
         Rope {
-            head: (0, 0),
-            tail: (0, 0),
-            visited: [(0, 0)].into()
+            knots: vec![Pos(0, 0); n],
+            visited: [Pos(0, 0)].into(),
         }
     }
 
-    fn x_diff(&self) -> isize {
-        (self.head.0 - self.tail.0).abs()
-    }
-
-    fn y_diff(&self) -> isize {
-        (self.head.1 - self.tail.1).abs()
-    }
-
-    fn move_x(&mut self, mv: isize) {
-        let dir = mv.signum();
+    fn perform_move(&mut self, mv: Pos) {
+        let knots = &mut self.knots;
         for _ in 0..mv.abs() {
-            self.head.0 += dir;
-            if self.x_diff() > 1 {
-                self.tail.0 += dir;
-                self.tail.1 = self.head.1;
-                self.visited.insert(self.tail);
+            knots[0] += mv.unit();
+            for i in 1..knots.len() {
+                let diff = knots[i - 1] - knots[i];
+                if diff.0.abs() * diff.1.abs() > 1 {
+                    knots[i] += diff.unit();
+                } else if diff.0.abs() > 1 {
+                    knots[i].0 += diff.0.signum();
+                } else if diff.1.abs() > 1 {
+                    knots[i].1 += diff.1.signum();
+                }
             }
-        }
-    }
-
-    fn move_y(&mut self, mv: isize) {
-        let dir = mv.signum();
-        for _ in 0..mv.abs() {
-            self.head.1 += dir;
-            if self.y_diff() > 1 {
-                self.tail.1 += dir;
-                self.tail.0 = self.head.0;
-                self.visited.insert(self.tail);
-            }
-        }
-    }
-
-    fn perform_move(&mut self, mv: Move) {
-        match mv {
-            Move(x, 0) => self.move_x(x),
-            Move(0, y) => self.move_y(y),
-            _ => panic!("Invalid move!")
+            self.visited.insert(knots[knots.len() - 1]);
         }
     }
 }
 
-fn main() {
-    //let input = TEST;
-    let input = std::fs::read_to_string("inputs/day9.in").unwrap();
-    let moves = input.lines().map(Move::from).collect_vec();
-    let mut rope = Rope::new();
-    for mv in moves {
+fn solve1(moves: &Vec<Pos>) {
+    let mut rope = Rope::new(2);
+    for &mv in moves {
         rope.perform_move(mv);
     }
-    //println!("{:?}", rope.visited);
-    println!("{}", rope.visited.len())
+    println!("Solution for problem 1: {}", rope.visited.len());
+}
+
+fn solve2(moves: &Vec<Pos>) {
+    let mut rope = Rope::new(10);
+    for &mv in moves {
+        rope.perform_move(mv);
+    }
+    println!("Solution for problem 2: {}", rope.visited.len());
+}
+
+fn main() {
+    let input = std::fs::read_to_string("inputs/day9.in").unwrap();
+    let moves = input.lines().map(Pos::from).collect_vec();
+    solve1(&moves);
+    solve2(&moves);
 }
